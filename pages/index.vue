4a117abb0cf9c5bbe2931b444a9490002c6fe35a
Elvin Chu <elvincth@gmail.com>
2020-10-19 12:26:35 +0800

    to light mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
-
-
+<i>hello</i>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+
   }
 }
 </script>
