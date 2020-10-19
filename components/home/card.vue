e8f56289c432acdbba9365bb3294bd0e22bc32b5
Elvin Chu <elvincth@gmail.com>
2020-10-19 14:32:05 +0800

    Added card components

---
+++ components/home/card.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    color: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
